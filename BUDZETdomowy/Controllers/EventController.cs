3d4486f02d3439685a3192cf22218d0f90a713a3
wyszynek <wyszynski.jakub03@gmail.com>
2024-05-26 19:52:50 +0200

    komentarze w kodzie
    wyjasnienie jak dziala skrypt kalendarza w Event/Index

--- BUDZETdomowy/Controllers/EventController.cs
+++ BUDZETdomowy/Controllers/EventController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using HomeBudget.Data;
 using HomeBudget.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace HomeBudget.Controllers
 {
+    [Authorize]
     public class EventController : Controller
     {
         private readonly ApplicationDbContext _context;

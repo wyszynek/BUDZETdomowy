fce4e870d448150e6acd1c2761e22bbd790739b8
wyszynek <wyszynski.jakub03@gmail.com>
2024-05-30 15:58:52 +0200

    przekierowanie edit

--- BUDZETdomowy/Controllers/TransactionController.cs
+++ BUDZETdomowy/Controllers/TransactionController.cs
@@ -260,6 +260,59 @@ namespace HomeBudget.Controllers
             return View(transaction);
         }
 
+        // GET: Transaction/Edit/5
+        public async Task<IActionResult> EditFundsFromSource(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var transaction = await _context.Transactions.FindAsync(id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            var sourceOfIncomes = await _context.SourceOfIncomes.ToListAsync();
+            ViewBag.SourceOfIncomes = new SelectList(sourceOfIncomes, "Id", "Name");
+
+            PopulateCategoriesAndAccounts();
+            return View(transaction);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditFundsFromSource(int id, Transaction transaction)
+        {
+            if (id != transaction.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // Zapisz zmiany w transakcji
+                    _context.Update(transaction);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TransactionExists(transaction.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(transaction);
+        }
 
         // GET: Transaction/Edit/5
         public async Task<IActionResult> Edit(int? id)
@@ -274,6 +327,18 @@ namespace HomeBudget.Controllers
             {
                 return NotFound();
             }
+
+            var categoryIcon = _context.Categories
+                .Where(c => c.Id == transaction.CategoryId)
+                .Select(c => c.Icon)
+                .FirstOrDefault();
+
+            if (categoryIcon == "&#128184;")
+            {
+                // Jeśli kategoria jest specjalna, przekieruj użytkownika na inny widok
+                return RedirectToAction("EditFundsFromSource", new { id = transaction.Id });
+            }
+
             PopulateCategoriesAndAccounts();
             return View(transaction);
         }
@@ -375,7 +440,8 @@ namespace HomeBudget.Controllers
                             return RedirectToAction(nameof(Index));
                         }
                     }
-                        _context.Update(transaction);
+
+                    _context.Update(transaction);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)

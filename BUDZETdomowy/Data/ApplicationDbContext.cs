a78894118fa7dce49e9e072ee83dead71bc268c3
wyszynek <wyszynski.jakub03@gmail.com>
2024-05-31 14:43:40 +0200

    wiecej walut w bazie
    nie trzeba juz wpisywac z palca

--- BUDZETdomowy/Data/ApplicationDbContext.cs
+++ BUDZETdomowy/Data/ApplicationDbContext.cs
@@ -81,6 +81,19 @@ namespace HomeBudget.Data
                 .WithMany()
                 .HasForeignKey(t => t.UserId)
                 .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Currency>().HasData(
+                new Currency { Id = 1, Code = "PLN" },
+                new Currency { Id = 2, Code = "EUR" },
+                new Currency { Id = 3, Code = "GBP" },
+                new Currency { Id = 4, Code = "USD" },
+                new Currency { Id = 5, Code = "CHF" },
+                new Currency { Id = 6, Code = "CAD" },
+                new Currency { Id = 7, Code = "AUD" },
+                new Currency { Id = 8, Code = "JPY" },
+                new Currency { Id = 9, Code = "CZK" },
+                new Currency { Id = 10, Code = "SEK" }
+            );
         }
 
         public DbSet<Transaction> Transactions { get; set; }
---
+++ BUDZETdomowy/Migrations/20240531123206_currenciesInData.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HomeBudget.Migrations
+{
+    /// <inheritdoc />
+    public partial class currenciesInData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Currencies",
+                columns: new[] { "Id", "Code" },
+                values: new object[,]
+                {
+                    { 1, "PLN" },
+                    { 2, "EUR" },
+                    { 3, "GBP" },
+                    { 4, "USD" },
+                    { 5, "CHF" },
+                    { 6, "CAD" },
+                    { 7, "AUD" },
+                    { 8, "JPY" },
+                    { 9, "CZK" },
+                    { 10, "SEK" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+    }
+}
